325946fd92dd4adb4a46f7287e8dbf26cb364bd2
Christian Ortega <christianmortega7@gmail.com>
2020-04-24 15:49:49 -0400

    Implement function for playing a single round

--- js/script.js
+++ js/script.js
@@ -1,12 +1,63 @@
-function playRound(playerSelection, computerSelection) {
+function game(numberOfRounds) 
+{
+    let playerSelection;
+    let computerSelection;
+    
+    for(let i = 0; i < numberOfRounds; i++) 
+    {
+        playerSelection = playerPlay();
+        while(!verifyPlayerSelection(playerSelection))
+            playerSelection = playerPlay(false);
+        computerSelection = computerPlay();
+        alert(playRound(playerSelection, computerSelection));
 
+    }
 }
 
-function computerPlay() {
+function playRound(playerSelection, computerSelection) 
+{
+    let result = `You chose: ${playerSelection}. Computer chose: ${computerSelection}.`;
+
+    if(playerSelection == computerSelection) 
+    {
+        result += `\nIt's a tie...`;
+    }
+    else if( (playerSelection == "Rock" && computerSelection == "Scissors") || 
+             (playerSelection == "Paper" && computerSelection == "Rock") || 
+             (playerSelection == "Scissors" && computerSelection == "Paper") ) 
+    {
+        result += `\nYou Win! ${playerSelection} beats ${computerSelection}.`;
+    }
+    else 
+    {
+        result += `\nYou Lose! ${computerSelection} beats ${playerSelection}.`
+    }
+
+    return result;
+}
+
+function playerPlay(isValidEntry = true) 
+{
+    let playerChoice = isValidEntry ? prompt("Please select your choice: Rock, Paper, or Scissors") : prompt("Invalid Choice. Please select your choice: Rock, Paper, or Scissors");
+    playerChoice = playerChoice.toSentenceCase();
+    return playerChoice;
+}
+
+function verifyPlayerSelection(playerSelection) 
+{
+    if(playerSelection == "Rock" || playerSelection == "Paper" || playerSelection == "Scissors")
+        return true;
+    //else
+    return false;
+}
+
+function computerPlay()
+{
     let numericalChoice = Math.floor(Math.random() * 3);
     let finalChoice = null;
 
-    switch(numericalChoice) {
+    switch(numericalChoice)
+    {
         case 0:
             finalChoice = "Rock";
             break;
@@ -23,12 +74,9 @@ function computerPlay() {
     return finalChoice;
 }
 
-function playerPlay() {
-    let playerChoice = prompt("Please select your choice: Rock, Paper, or Scissors");
-    playerChoice = playerChoice.toSentenceCase();
-    return playerChoice;
-}
-
-String.prototype.toSentenceCase = function() {
+String.prototype.toSentenceCase = function()
+{
     return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
 }
+
+game(1);
