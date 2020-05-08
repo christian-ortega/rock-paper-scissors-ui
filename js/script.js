2456b98c7e6538dd83355677d956bc5879275b42
Christian Ortega <christianmortega7@gmail.com>
2020-05-08 01:10:42 -0400

    Add functionality to update game info UI on button press

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const maxRounds = 5;
+let currentRoundNumber = 0;
+let previousResult = -2;
+
+
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -5,76 +10,49 @@ buttons.forEach((button) => {
         if(button.id == "rock") playerSelection = "Rock";
         if(button.id == "paper") playerSelection = "Paper";
         if(button.id == "scissors") playerSelection = "Scissors";
-        game(1, playerSelection);
+        
+        if(currentRoundNumber < maxRounds || previousResult == 0) {
+            updateRoundNumber();
+            previousResult = playRound(playerSelection);
+        } 
+        
+        if(currentRoundNumber == maxRounds && (previousResult == 1 || previousResult == -1)) {
+            gameOver();
+            previousResult = -3;
+        }
     });
 });
 
-function game(numberOfRounds, playerSelection) 
+function playRound(playerSelection) 
 {
-    let computerSelection;
-    let message;
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    for(let i = 0; i < numberOfRounds; i++) 
-    {
-        computerSelection = computerPlay();
+    let computerSelection = computerPlay();
+    const roundResults = document.querySelector('#roundresults');
+    const playerScoreNumber = document.querySelector('#playerscorenumber');
+    const computerScoreNumber = document.querySelector('#computerscorenumber');
 
-        roundResults = playRound(playerSelection, computerSelection);
-        if(roundResults.score == 1) 
-            playerScore++;
-        else if(roundResults.score == -1)
-            computerScore++;
+    updateImage('player', playerSelection);
+    updateImage('computer', computerSelection);
 
-        message = `ROUND ${i + 1}:\n`;
-        message += roundResults.message;
-        message += `\n\n Player Score: ${playerScore} Computer Score: ${computerScore}`
-
-        if(roundResults.score == 0)
-            i--;
-
-        if(i == numberOfRounds - 1)
-        {
-            if(playerScore == computerScore)
-                message += `\n\n GAME OVER... It's a tie!?`;
-            else if(playerScore < computerScore)
-                message += `\n\n GAME OVER... You lost...`;
-            else if(playerScore > computerScore)
-                message += `\n\n GAME OVER... You won!!`
-        }
-
-        alert(message);
-        console.log(message);
-
-    }
-}
-
-function playRound(playerSelection, computerSelection) 
-{
-    let result = {
-        message: `You chose: ${playerSelection}. Computer chose: ${computerSelection}.`,
-        score: 0
-    }
 
     if(playerSelection == computerSelection) 
     {
-        result.message += `\nIt's a tie... Try again.`;
-        result.score = 0;
+        roundResults.textContent = `It's a tie... Try again.`;
+        return 0;
     }
     else if( (playerSelection == "Rock" && computerSelection == "Scissors") || 
              (playerSelection == "Paper" && computerSelection == "Rock") || 
              (playerSelection == "Scissors" && computerSelection == "Paper") ) 
     {
-        result.message += `\nYou Win! ${playerSelection} beats ${computerSelection}.`;
-        result.score = 1;
+        roundResults.textContent = `You Win This Round! ${playerSelection} beats ${computerSelection}.`;
+        playerScoreNumber.textContent = parseInt(playerScoreNumber.textContent) + 1;
+        return 1;
     }
     else 
     {
-        result.message += `\nYou Lose! ${computerSelection} beats ${playerSelection}.`
-        result.score = -1;
+        roundResults.textContent = `You Lose This Round! ${computerSelection} beats ${playerSelection}.`
+        computerScoreNumber.textContent = parseInt(computerScoreNumber.textContent) + 1;
+        return -1;
     }
-
-    return result;
 }
 
 function computerPlay()
@@ -98,4 +76,55 @@ function computerPlay()
     }
 
     return finalChoice;
+}
+
+function updateImage(user, selection) {
+    let ourImage;
+    if(user == 'player')
+        ourImage = document.querySelector('#playerchoiceimage');
+    else if(user == 'computer')
+        ourImage = document.querySelector('#computerchoiceimage');
+    else
+        return;
+
+    if(selection == "Rock")
+        ourImage.src = "img/rock.jpg";
+    else if(selection == "Paper")
+        ourImage.src = "img/paper.jpg";
+    else if(selection == "Scissors")
+        ourImage.src = "img/scissors.jpg";
+}
+
+function updateRoundNumber() {
+    const roundNumber = document.querySelector('#roundnumber');
+    
+    if(previousResult < -1 || previousResult > 1) {
+        currentRoundNumber = 1;
+        roundNumber.textContent = "ROUND 1";
+    }
+    else if(previousResult == 0) {
+        return;
+    }
+    else {
+        currentRoundNumber++;
+        roundNumber.textContent = `ROUND ${currentRoundNumber}`;
+    }
+}
+
+function gameOver() {
+    const playerScoreNumber = document.querySelector('#playerscorenumber');
+    const computerScoreNumber = document.querySelector('#computerscorenumber');
+    const roundResults = document.querySelector('#roundresults');
+
+
+    let playerScore = parseInt(playerScoreNumber.textContent);
+    let computerScore = parseInt(computerScoreNumber.textContent);
+
+    if(playerScore == computerScore)
+        roundResults.textContent += `\n\n GAME OVER... It's a tie!?`;
+    else if(playerScore < computerScore)
+        roundResults.textContent += `\n\n GAME OVER... You lost...`;
+    else if(playerScore > computerScore)
+        roundResults.textContent += `\n\n GAME OVER... You won!!`
+
 }
